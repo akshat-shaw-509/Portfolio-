4bb85f77b848e1923fe6a5357c3afe59e20c31f4
akshat-shaw-509 <akshatshaw99@gmail.com>
2024-10-25 14:13:15 +0530

    Create script.js

---
+++ script.js
@@ -0,0 +1,10 @@
+window.onscroll = function() {
+    var header = document.getElementById("header");
+    var sticky = header.offsetTop;
+
+    if (window.pageYOffset > sticky) {
+        header.classList.add("sticky");
+    } else {
+        header.classList.remove("sticky");
+    }
+}
